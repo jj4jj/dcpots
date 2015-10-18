e3874096125be702d14518ca13324c9961a22fd8
jj4jj <ubi@thinkingforever.com>
2015-10-18 19:12:03 +0800

    ""

--- tools/git_add.sh
+++ tools/git_add.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 if [[ "$1" == "" ]];then 
     echo "not found commit msg , must add !"
     exit 0
 fi
-uname -v| grep "Ubuntu"
+uname -v | grep "Ubuntu"
 if [[ "$?" == "0" ]];then
 git status | grep "modified" | awk '{print "git add",$3}' | sh
 else
