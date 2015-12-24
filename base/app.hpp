9170d4f4aae8a134811fdfb171c7fd18f7a4e55e
jj4jj <resc@vip.qq.com>
2015-12-24 17:47:22 +0800

    add extra

--- base/app.hpp
+++ base/app.hpp
@@ -8,7 +8,28 @@ app.set_name();
 app.bind("tcp://xxx");
 app.run_every(func,10ms);
 app.event();
+
+//general 
+//log
+//random
+//io(net/file)
+//timer
+//debug
+//signal trap
+//console
+//status
+//control
+    //start,stop,reload,restart
+//vm
+
+
+
+
+
+
 */
+
+
 
 
 
