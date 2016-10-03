f434c7a9e600a88904b898d8a03aedf6c9fa9d0a
rescee <rescee@RESCEE-PC>
2016-10-03 19:01:13 +0800

    add collections of mempool and hashmap

--- base/dccollections.cpp
+++ base/dccollections.cpp
@@ -1,10 +1,43 @@
-
-
+#include "logger.h"
+#include "dcmath.hpp"
 #include "dccollections.hpp"
+///////////////////////////////////////////////////////////////////////////////////////////
+#define MMPOOL_MAGIC	("mmpool-magic")
+#define DIV_CEIL(s,n)		(((s)+(n-1))/n)
+#define ALIGN_N(s,n)		(((s)+(n-1))/n*n)
 
+///////////////////////////////////////////////////////////////////////////////////////////
 namespace dcs {
 struct mempool_impl_t {
-    mempool_conf_t  conf;    
+	mempool_conf_t::strategy    stg;
+	size_t						data_size;
+	size_t						block_size;
+	size_t						block_max;
+	char						magic[16];
+	size_t                      used;
+	size_t						last_alloc_id;
+	size_t						head_begin;
+	size_t						data_begin;
+	size_t						data_end;
+};
+union mempool_impl_head {
+	///////////////////////////////////////////
+	uint64_t				bmp[1];
+	///////////////////////////////////////////
+	struct block_list_t {
+		struct queue_t {
+			size_t			front;
+			size_t			rear;
+		};
+		struct entry_t {
+			uint32_t		busy:1;
+			uint32_t		prev:31;
+			uint32_t		next;
+		};
+		queue_t				free;
+		queue_t				busy;
+		entry_t				list[1];
+	} bkl;
 };
 mempool_conf_t::mempool_conf_t(){
     data = nullptr;
@@ -12,82 +45,489 @@ mempool_conf_t::mempool_conf_t(){
     stg = MEMPOOL_STRATEGY_BITMAP;
     attach = false;
 }
+static inline int mmpool_bitmap_init(uint64_t bmp[], size_t blksz, size_t nblk, bool attach) {
+	return 0;
+}
+static inline int mmpool_blklist_init(mempool_impl_head::block_list_t * bkl, size_t blksz, size_t nblk, bool attach) {
+	if (!attach){
+		memset(bkl, 0, sizeof(*bkl));
+		bkl->free.front = bkl->free.rear = 0;
+		bkl->busy.front = bkl->busy.rear = 0;
+		//init construct a linked list free
+		bkl->list[0].next = 1;
+		bkl->list[0].prev = nblk-1;
+		for (size_t i = 1; i < nblk; ++i) {
+			bkl->list[i].prev = i-1;
+			bkl->list[i].next = (i+1)%nblk;
+		}
+	}
+	return 0;
+}
 int     mempool_t::init(const mempool_conf_t & conf){
-    //1. check size match ?
-    //2. check attach ?
-    //3. dispatch strategy 
-    //4. impl
+	size_t total_size = size(conf);
+	if (total_size == 0 || conf.data_size < total_size) {
+		GLOG_ERR("config data size :%zu need %zu error !", conf.data_size, total_size);
+		return -1;
+	}
+	impl_ = (mempool_impl_t*)conf.data;
+	mempool_impl_head * mphead = (mempool_impl_head*)&(impl_[1]);
+	if (conf.attach) {
+		if (!strcmp(MMPOOL_MAGIC, impl_->magic)) {
+			GLOG_ERR("mmpool magic head error [%s] !", impl_->magic);
+			return -1;
+		}
+		if (!strcmp(MMPOOL_MAGIC, (char *)impl_ + impl_->data_end)) {
+			GLOG_ERR("mmpool magic tail error [%s] !", (char *)impl_ + impl_->data_end);
+			return -1;
+		}
+	}
+	else {
+		memset(impl_, 0, sizeof(*impl_));
+		impl_->block_max = conf.block_max;
+		impl_->data_size = conf.data_size;
+		impl_->block_size = conf.block_size;
+		impl_->stg = conf.stg;
+		impl_->head_begin = sizeof(mempool_impl_t);
+		if (conf.stg == mempool_conf_t::MEMPOOL_STRATEGY_BITMAP) {
+			impl_->data_begin = impl_->head_begin + sizeof(uint64_t)*DIV_CEIL(conf.block_max,64);
+		}
+		else if (conf.stg == mempool_conf_t::MEMPOOL_STRATEGY_BLKLST) {
+			impl_->data_begin = impl_->head_begin + \
+				sizeof(mempool_impl_head::block_list_t)+\
+				sizeof(mempool_impl_head::block_list_t::entry_t)*(conf.block_max-1);
+		}
+		else {
+			assert("not support now !" && false);
+		}
+		impl_->data_end = impl_->data_begin + conf.block_max*conf.block_size;
+		strcpy(impl_->magic, MMPOOL_MAGIC);
+		strcpy((char *)impl_ + impl_->data_end, MMPOOL_MAGIC);
+	}
+	if (conf.stg == mempool_conf_t::MEMPOOL_STRATEGY_BITMAP) {
+		return mmpool_bitmap_init(mphead->bmp, conf.block_size, conf.block_max, conf.attach);
+	}
+	else if (conf.stg == mempool_conf_t::MEMPOOL_STRATEGY_BLKLST) {
+		return mmpool_blklist_init(&mphead->bkl, conf.block_size, conf.block_max, conf.attach);
+	}
+	else {
+		GLOG_ERR("not support mempool strategy type:%d",conf.stg);
+		return -1;
+	}
     return 0;
 }
 void *  mempool_t::alloc(){
-    //todo
-    return nullptr;
+	void * p = nullptr;
+	if (impl_->used >= impl_->block_max) {
+		return nullptr;
+	}
+	if (impl_->stg == mempool_conf_t::MEMPOOL_STRATEGY_BITMAP) {
+		size_t idx = impl_->last_alloc_id/64;
+		uint64_t * bmp = ((mempool_impl_head*)(&impl_[1]))->bmp;
+		size_t bitmap_count = impl_->block_max/64;
+		//find first 0
+		size_t ffo = 0;
+		for (size_t i = 0; i < bitmap_count; ++i, idx = (idx+1)% bitmap_count) {			
+			if ((ffo = __builtin_ffsll(~(bmp[idx])))) {
+				break;
+			}
+		}
+		assert(ffo);
+		p = (char*)impl_ + impl_->data_begin + (idx*64+ffo)*impl_->block_size;
+		bmp[idx] |= (1ULL << (ffo - 1));//set 1
+	}
+	else if (impl_->stg == mempool_conf_t::MEMPOOL_STRATEGY_BLKLST) {
+		//free head alloc
+		mempool_impl_head::block_list_t * bkl = &((mempool_impl_head*)(&impl_[1]))->bkl;
+		size_t idx = bkl->free.front;
+		assert(0 == bkl->list[idx].busy);
+		//erase from free
+		bkl->list[bkl->list[idx].prev].next = bkl->list[idx].next;
+		bkl->list[bkl->list[idx].next].prev = bkl->list[idx].prev;
+		//append to busy
+		bkl->list[idx].next = bkl->busy.front;
+		bkl->list[idx].prev = bkl->busy.rear;
+		bkl->list[bkl->busy.rear].next = idx;
+		bkl->list[idx].busy = 1;
+		p = (char*)impl_ + impl_->data_begin + idx*impl_->block_size;
+	}
+	if (p) {
+		++impl_->used;
+		impl_->last_alloc_id = this->id(p);
+	}
+    return p;
 }
-void    mempool_t::free(void *){
-    //todo
+void    mempool_t::free(void * p){
+	int ret = -1;
+	if (impl_->used == 0) {
+		return;
+	}
+	size_t zid = this->id(p);
+	if (zid == 0) {
+		return;
+	}
+	size_t idx = zid - 1;
+	if (impl_->stg == mempool_conf_t::MEMPOOL_STRATEGY_BITMAP) {
+		uint64_t * bmp = ((mempool_impl_head*)(&impl_[1]))->bmp;
+		uint64_t  mask = (1ULL << (idx % 64));
+		if (bmp[idx / 64] & mask) {
+			bmp[idx / 64] &= ~(mask);
+			ret = 0;
+		}
+		else {
+			ret = -1;
+		}
+	}
+	else if (impl_->stg == mempool_conf_t::MEMPOOL_STRATEGY_BLKLST) {
+		mempool_impl_head::block_list_t * bkl = &((mempool_impl_head*)(&impl_[1]))->bkl;
+		if (1 == bkl->list[idx].busy) {
+			//erase from busy
+			bkl->list[bkl->list[idx].prev].next = bkl->list[idx].next;
+			bkl->list[bkl->list[idx].next].prev = bkl->list[idx].prev;
+			//append into free
+			bkl->list[idx].prev = bkl->free.rear;
+			bkl->list[idx].next = bkl->free.front;
+			bkl->list[idx].busy = 0;
+			bkl->list[bkl->free.rear].next = idx;
+			bkl->free.rear = idx;
+		}
+		else {
+			ret = -1;
+		}
+	}
+	if (0 == ret) {
+		--impl_->used;
+	}
 }
 void *  mempool_t::ptr(size_t id){
-    //todo
-    return nullptr;
+	if (impl_->used == 0 || id == 0 || id > impl_->block_max) {
+		return nullptr;
+	}
+	return (char*)impl_ + impl_->data_begin + (id-1)*impl_->block_size;
 }
-size_t  mempool_t::id(void *) const {
-    //todo
-    return 0;
+size_t  mempool_t::id(void * p) const {
+	if (impl_->used == 0 || 
+		p < (char*)impl_ + impl_->data_begin ||
+		p >= (char*)impl_ + impl_->data_end) {
+		return 0;
+	}
+	return ((char*)p - (char*)impl_ - impl_->data_begin) / impl_->block_size + 1;
 }
 size_t  mempool_t::used() const {
-    //todo
-    return 0;
+	return impl_->used;
 }
 size_t  mempool_t::capacity() const {
-    //todo
-    return 0;
+    return impl_->block_max;
 }
-size_t  mempool_t::size(const mempool_conf_t & conf){
-    //todo
-    return 0;
+void *  mempool_t::next(void * blk) {
+	if (impl_->stg == mempool_conf_t::MEMPOOL_STRATEGY_BITMAP) {
+		uint64_t * bmp = ((mempool_impl_head*)(&impl_[1]))->bmp;
+		size_t blkid = this->id(blk);//+1-1 . NEXT
+		size_t blkidx = blkid/64;
+		size_t bitidx = blkid%64;
+		size_t bitmap_count = impl_->block_max / 64;
+		//find first 1
+		size_t ffo = 0;
+		for (; blkidx < bitmap_count; ++blkidx) {
+			for (int i = bitidx; i < 64; ++i) {
+				if (bmp[blkid] & (1ULL << i)) {
+					return this->ptr(blkidx*64+i+1);
+				}
+			}
+		}
+		return nullptr;
+	}
+	else if (impl_->stg == mempool_conf_t::MEMPOOL_STRATEGY_BLKLST) {
+		mempool_impl_head::block_list_t * bkl = &((mempool_impl_head*)(&impl_[1]))->bkl;
+		size_t blkidx = this->id(blk) - 1;
+		if (blkidx == bkl->busy.rear) {
+			return nullptr;
+		}
+		assert(1 == bkl->list[blkidx].busy);
+		blkidx = bkl->list[blkidx].next;
+		return this->ptr(blkidx+1);
+	}
+	return nullptr;
+}
+size_t  mempool_t::size(mempool_conf_t::strategy stg, size_t nblk, size_t blksz){
+	if (nblk == 0 || blksz == 0) {
+		return 0;
+	}
+	if (stg == mempool_conf_t::MEMPOOL_STRATEGY_BITMAP) {
+		return ALIGN_N(sizeof(mempool_impl_t) + nblk* blksz + 16 +\
+			sizeof(uint64_t) * DIV_CEIL(nblk, 64), 16);
+	}
+	else if (stg == mempool_conf_t::MEMPOOL_STRATEGY_BLKLST) {
+		return ALIGN_N(sizeof(mempool_impl_t) + nblk* blksz + 16 +\
+			sizeof(mempool_impl_head::block_list_t) + \
+			sizeof(mempool_impl_head::block_list_t::entry_t)*(nblk-1), 16);
+	}
+	else {		
+		return 0;
+	}
 }
 mempool_t::mempool_t(){
     impl_ = nullptr;
 }
-
-
-/////////////////////////////////////////////////////////////////////////////////////
-
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 hashmap_conf_t::hashmap_conf_t() {
+	data = nullptr;
     init = nullptr;
     hash = nullptr;
     comp = nullptr;
+	data_size = block_max = block_size = 0;
+	attach = false;
+	level = 3;
 }
-int         hashmap_t::init(const mempool_conf_t & conf){
-    //todo
-    return 0;
+#define  HASHMAP_MAGIC		("hashmap-magic")
+#define HASHMAP_MAX_LEVEL	(11)
+//multi level hash
+//cmax(count)
+struct hashmap_impl_t {
+	hashmap_conf_t::block_init  init;
+	hashmap_conf_t::block_hash  hash;
+	hashmap_conf_t::block_comp  comp;
+	mempool_t					mmpool;
+	char						magic[16];
+	int							level;
+	size_t						level_offset[HASHMAP_MAX_LEVEL];
+	size_t						level_size[HASHMAP_MAX_LEVEL];
+	size_t						table_start;
+	size_t						table_size;
+	size_t						mmpool_start;
+	size_t						mmpool_end;
+	//////////////////////////////////////////////////////////
+	struct table_item_t {
+		size_t		code;
+		size_t		id;
+	};
+	struct layout_desc {
+		table_item_t			table[1];
+		char					mmpool[1];
+		char					magic[16];
+	};
+};
+int         hashmap_t::init(const hashmap_conf_t & conf){
+	size_t total_size = size(conf.level, conf.block_max, conf.block_size);
+	if (total_size == 0 || conf.data_size < total_size) {
+		return -1;
+	}
+	impl_ = (hashmap_impl_t *)conf.data;
+	if (conf.attach) {
+		impl_->comp = conf.comp;
+		impl_->init = conf.init;
+		impl_->hash = conf.hash;
+		//check
+		if (strcmp(impl_->magic, HASHMAP_MAGIC)) {
+			return -1;
+		}
+		if (strcmp((char*)impl_ + impl_->mmpool_end, HASHMAP_MAGIC)) {
+			return -1;
+		}
+		if (impl_->level != conf.level) {
+			return -1;
+		}
+	}
+	else {
+		memset(impl_, 0, sizeof(*impl_));
+		impl_->comp = conf.comp;
+		impl_->init = conf.init;
+		impl_->hash = conf.hash;
+		size_t prime_n = dcs::prime_next(conf.block_max);
+		impl_->level = conf.level;
+		impl_->level_size[conf.level-1] = prime_n;
+		size_t table_size = prime_n;
+		for (int i = conf.level-2;i >= 0; ++i) {
+			prime_n = dcs::prime_prev(prime_n);
+			impl_->level_size[i] = prime_n;
+			table_size += prime_n;
+		}
+		for (int i = 1; i < conf.level; ++i) {
+			impl_->level_offset[i] = impl_->level_offset[i-1] + impl_->level_size[i-1];
+		}
+		//code -> id
+		hashmap_impl_t::table_item_t * table = (hashmap_impl_t::table_item_t*)((char*)impl_ + impl_->table_start);
+		memset(table, 0 , sizeof(*table)*table_size);
+		impl_->table_start = sizeof(hashmap_impl_t);
+		impl_->table_size = table_size;
+		impl_->mmpool_start = impl_->table_start + sizeof(*table)*table_size;
+		impl_->mmpool_end = impl_->mmpool_start + mempool_t::size(mempool_conf_t::MEMPOOL_STRATEGY_BITMAP, conf.block_max, conf.block_size);
+		strcpy(impl_->magic, HASHMAP_MAGIC);
+		strcpy((char*)impl_ + impl_->mmpool_end, HASHMAP_MAGIC);
+	}
+	mempool_conf_t mpc;
+	mpc.stg = mempool_conf_t::MEMPOOL_STRATEGY_BITMAP;
+	mpc.block_max = conf.block_max;
+	mpc.block_size = conf.block_size;
+	mpc.attach = conf.attach;
+	mpc.data = (char*)impl_ + impl_->mmpool_start;
+	mpc.data_size = conf.data_size - impl_->mmpool_start;
+    return impl_->mmpool.init(mpc);
 }
 void *      hashmap_t::insert(const void * blk, bool unique){
-    //todo
-    return nullptr;
+	if (unique) {
+		void * p = find(blk);
+		if (p) {
+			return nullptr;
+		}
+	}
+	void * p = impl_->mmpool.alloc();
+	if (!p) {
+		return nullptr;
+	}
+	hashmap_impl_t::table_item_t * table = (hashmap_impl_t::table_item_t*)((char*)impl_ + impl_->table_start);
+	size_t id = impl_->mmpool.id(p);
+	size_t code = impl_->hash(blk);
+	size_t lidx = 0;
+	bool inserted = false;
+	for (int i = 0;i < impl_->level; ++i) {
+		lidx = impl_->level_offset[i] + code % impl_->level_size[i];
+		if (table[lidx].id == 0) {
+			table[lidx].id = id;
+			table[lidx].code = code;
+			inserted = true;
+			break;
+		}
+	}
+	if (!inserted) {
+		//last layer
+		size_t retry = capacity();
+		while (table[lidx].id && retry-->0) {
+			 ++lidx;
+			 if (lidx >= impl_->level_offset[impl_->level - 1] + impl_->level_size[impl_->level - 1]) {
+				 lidx = impl_->level_offset[impl_->level - 1];
+			 }
+		}
+		if (table[lidx].id) {
+			impl_->mmpool.free(p);
+			p = nullptr;
+			assert("no allocated space !" && false);
+		}
+		else {
+			table[lidx].id = id;
+			table[lidx].code = code;
+		}
+	}
+	if (p && impl_->init) {
+		impl_->init(p);
+	}
+    return p;
 }
+static inline size_t _list_tail(hashmap_impl_t * impl_, size_t code) {
+	size_t lidx = -1, plidx = -1;
+	hashmap_impl_t::table_item_t * table = (hashmap_impl_t::table_item_t*)((char*)impl_ + impl_->table_start);
+	for (int i = 0; i < impl_->level; ++i) {
+		plidx = lidx;
+		lidx = impl_->level_offset[i] + code % impl_->level_size[i];
+		if (table[lidx].id == 0) {
+			return plidx;
+		}
+	}
+	//last layer
+	size_t retry = impl_->mmpool.capacity();
+	while (table[lidx].id && retry-->0) {
+		plidx = lidx;
+		++lidx;
+		if (lidx >= impl_->level_offset[impl_->level - 1] + impl_->level_size[impl_->level - 1]) {
+			lidx = impl_->level_offset[impl_->level - 1];
+		}
+	}
+	if (table[lidx].id) {
+		assert("no allocated space !" && false);
+		return -1;
+	}
+	else {
+		return plidx;
+	}
+}
+static inline void * _list_find(size_t & lidx, hashmap_impl_t * impl_, size_t code, const void * blk) {
+	size_t retry = impl_->mmpool.capacity();
+	hashmap_impl_t::table_item_t * table = (hashmap_impl_t::table_item_t*)((char*)impl_ + impl_->table_start);
+	while (retry-- > 0 && table[lidx].id) {
+		if (table[lidx].code == code) {
+			void * p = impl_->mmpool.ptr(table[lidx].id);
+			if (0 == impl_->comp(blk, p)) {
+				return p;
+			}
+		}
+		++lidx;
+		if (lidx >= impl_->level_offset[impl_->level - 1] + impl_->level_size[impl_->level - 1]) {
+			lidx = impl_->level_offset[impl_->level - 1];
+		}
+	}
+	return nullptr;
+}
+static inline void * _find(size_t & level, size_t & lidx, hashmap_impl_t * impl_, const void * blk) {
+	size_t code = impl_->hash(blk);
+	hashmap_impl_t::table_item_t * table = (hashmap_impl_t::table_item_t*)((char*)impl_ + impl_->table_start);
+	lidx = 0;
+	level = 0;
+	for (int i = 0;i < impl_->level; ++i) {
+		lidx = impl_->level_offset[i] + code % impl_->level_size[i];
+		if (table[lidx].id) {
+			if (table[lidx].code == code) {
+				void * p = impl_->mmpool.ptr(table[lidx].id);
+				if (0 == impl_->comp(blk, p)) {
+					level = i;
+					return p;
+				}
+			}
+		}
+		else {
+			return nullptr;
+		}
+	}
+	assert(lidx >= impl_->level_offset[impl_->level - 1]);
+	level = impl_->level-1;
+	return _list_find(lidx, impl_, code, blk);
+}
+
 void *      hashmap_t::find(const void * blk){
-    //todo
-    return nullptr;
+	size_t lidx, level;
+    return _find(level, lidx, impl_, blk);
 }
 void        hashmap_t::remove(const void * blk){
+	size_t lidx, level;
+	void * p = _find(level, lidx, impl_, blk);
+	if (!p) {
+		return ;
+	}
+	hashmap_impl_t::table_item_t * table = (hashmap_impl_t::table_item_t*)((char*)impl_ + impl_->table_start);
+	size_t code = impl_->hash(blk);
+	size_t ltidx = _list_tail(impl_, code);
+	assert(ltidx != (size_t)-1);
+	if (ltidx != lidx) {
+		table[lidx] = table[ltidx];
+	}
+	table[ltidx].id = 0;
+	table[ltidx].code = 0;
+	impl_->mmpool.free(p);
 }
-void *      hashmap_t::next(void * blkprev){
-    //todo
-    return nullptr;
+void *      hashmap_t::next(void * blk){
+    return impl_->mmpool.next(blk);
 }
 size_t      hashmap_t::capacity() const {
-    //todo
-    return 0;
+	return impl_->mmpool.capacity();
 }
 size_t      hashmap_t::used() const {
-    //todo
-    return 0;
+    return impl_->mmpool.used();
 }
-size_t      hashmap_t::size(const hashmap_conf_t & conf){
-    //todo
-    return 0;
+size_t      hashmap_t::size(int level, size_t nblk, size_t blksz){
+	if (level < 1 || level > HASHMAP_MAX_LEVEL) {
+		return 0;
+	}
+	size_t mpsz = mempool_t::size(mempool_conf_t::MEMPOOL_STRATEGY_BITMAP, nblk, blksz);
+	if (mpsz == 0) {
+		return 0;
+	}
+	size_t prime_n = dcs::prime_next(nblk);
+	size_t table_size = prime_n;
+	for (int i = 1; i < level; ++i) {
+		prime_n = dcs::prime_prev(prime_n);
+		table_size += prime_n;
+	}
+	mpsz += table_size*sizeof(hashmap_impl_t::table_item_t);
+	mpsz += sizeof(hashmap_impl_t);
+    return mpsz;
 }
 
 
--- base/dccollections.hpp
+++ base/dccollections.hpp
@@ -1,16 +1,11 @@
 #pragma once
-
-#include "dcarray.hpp"
-#include "dcmmpool.hpp"
-#include "dchashtable.hpp"
-
 namespace dcs {
     //////////////////////dynamic/////////////////////////////////////////////
     struct mempool_impl_t;
     struct mempool_conf_t {
         enum strategy {
             MEMPOOL_STRATEGY_BITMAP = 0,
-            MEMPOOL_STRATEGY_LIST = 1,
+            MEMPOOL_STRATEGY_BLKLST = 1,
         };
         void *      data;
         size_t      data_size;
@@ -28,40 +23,44 @@ namespace dcs {
         size_t  id(void *) const;
         size_t  used() const;
         size_t  capacity() const;
-        static size_t  size(const mempool_conf_t & conf);
+		void *  next(void * blk);
+        static size_t  size(mempool_conf_t::strategy stg, size_t nblk, size_t blksz);
     public:
         mempool_t();
-        mempool_impl_t *   impl_;
+        mempool_impl_t *   impl_{nullptr};
     };
 
     ////////////////////////////////////////////////////////////////////
     struct hashmap_impl_t;
     struct hashmap_conf_t {
-        mempool_conf_t mm;
+		void *      data;
+		size_t      data_size;
+		size_t      block_size;
+		size_t      block_max;
+		bool        attach;
         typedef void(*block_init)(void * blk);
         typedef size_t(*block_hash)(const void * blk);
         typedef int(*block_comp)(const void * blkl, const void * blkr);
         block_init  init;
         block_hash  hash;
         block_comp  comp;
+		int			level;
         hashmap_conf_t();
     };
 
     struct hashmap_t {
-        int         init(const mempool_conf_t & conf);
+        int         init(const hashmap_conf_t & conf);
         void *      insert(const void * blk, bool unique = true);
         void *      find(const void * blk);
         void        remove(const void * blk);
-        void *      next(void * blkprev);
+        void *      next(void * blk);
         size_t      capacity() const;
         size_t      used() const;
-        static size_t   size(const hashmap_conf_t & conf);
+        static size_t   size(int level, size_t nblk, size_t blksz);
     public:
         hashmap_t();
         hashmap_impl_t  * impl_;
     };
-
-
 };
 
 
