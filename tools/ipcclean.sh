30d4a1a7d97416cc40769ed7d4cfc6fccd0fc45a
rescee <rescee@RESCEE-PC>
2016-10-04 16:42:03 +0800

    rm and rename files

--- tools/ipcclean.sh
+++ tools/ipcclean.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "clean shm list:"
+echo "clean empty shm list:"
 echo "----------------------------------------------"
 ipcs -m | awk '{if($6 == 0){print $0}}'
 ipcs -m | awk '{if($6 == 0){print "ipcrm -m",$2}}' | sh
@@ -9,7 +9,7 @@ echo "----------------------------------------------"
 
 
 if [ "$1" = "q" ];then
-echo "clean msgq list:"
+echo "clean empty msgq list:"
 echo "----------------------------------------------"
 ipcs -q | awk '{if($6 == 0){print $0}}'
 ipcs -q | awk '{if($6 == 0){print "ipcrm -q",$2}}' | sh
